534a36842b430e81fa56ad96b00b48d8eb7a94ef
G Sh <conan@yandex.ru>
2025-03-24 08:28:34 +0300

    добавил helper

---
+++ helper.py
@@ -0,0 +1,4 @@
+from faker import Faker
+fake = Faker()
+def generate_random_email():
+    return fake.email()
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,5 @@
-import pytest
-import random
-from faker import Faker
 
-fake = Faker()
+
 
 @pytest.fixture
-# фикстура, которая генерирует логин
-def generate_random_email():
-    return fake.email()
 
