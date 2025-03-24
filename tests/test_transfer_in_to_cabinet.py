ea218627dd734332c1b2d949dfd38ae313f52fc4
G Sh <conan@yandex.ru>
2025-03-25 00:16:39 +0300

    Добавил - проверку Выхода из аккаунта

--- tests/test_transfer_in_to_cabinet.py
+++ tests/test_transfer_in_to_cabinet.py
@@ -41,3 +41,24 @@ class TestTransferToCabinet:
         assert open_webdriver.find_element(By.XPATH, "//h1[text()='Соберите бургер']").text == 'Соберите бургер'
 
         close_webdriver
+
+    def test_logout_from_lk_logout_ok(self, open_webdriver, close_webdriver, enter_to_site):
+        #открываем главную страницу
+        enter_to_site
+        # нажимаем на кнопку Личный Кабинет на странице
+        open_webdriver.find_element(By.XPATH, "//p[text()='Личный Кабинет']").click()
+        # Добавь явное ожидание для загрузки страницы (проверяем появление надписи "В этом разделе вы можете изменить свои персональные данные")
+        WebDriverWait(open_webdriver, 3).until(
+            expected_conditions.visibility_of_element_located((By.XPATH, "//p[text()='В этом разделе вы можете изменить свои персональные данные']")))
+        # нажимаем на кнопку Выход на странице
+        open_webdriver.find_element(By.XPATH, "//button[text()='Выход']").click()
+
+        # Добавь явное ожидание для загрузки страницы (проверяем появление надписи "В этом разделе вы можете изменить свои персональные данные")
+        WebDriverWait(open_webdriver, 3).until(
+            expected_conditions.visibility_of_element_located(
+                (By.XPATH, "//h2[text()='Вход']")))
+
+        # Проверь, что текущий url равен 'https://stellarburgers.nomoreparties.site/account/profile'
+        assert open_webdriver.find_element(By.XPATH, "//h2[text()='Вход']").text == 'Вход'
+
+        close_webdriver
