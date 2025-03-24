9149fde4a154e3e5d6dad889cc09c657fe8ac1b1
G Sh <conan@yandex.ru>
2025-03-25 00:54:51 +0300

    Добавил - переходы к разделам:     «Соусы», «Начинки».

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -11,18 +11,28 @@ from locators import Locators
 class TestConstructor:
     def test_constructor_bulki_transfer_ok(self, open_webdriver, close_webdriver):
         #открываем главную страницу
-        #enter_to_site
         open_webdriver.get(constant_data.BASE_URL)
-        # нажимаем на кнопку Личный Кабинет на странице
+        # иммитируем переход на раздел Булки
         open_webdriver.find_element(By.XPATH, "//span[text()='Соусы']").click()
-
         open_webdriver.find_element(By.XPATH, "//span[text()='Булки']").click()
-        #time.sleep(5)
-        # Добавь явное ожидание для загрузки страницы (проверяем появление надписи "В этом разделе вы можете изменить свои персональные данные")
-        #WebDriverWait(open_webdriver, 3).until(
-         #   expected_conditions.visibility_of_element_located((By.XPATH, "//p[text()='В этом разделе вы можете изменить свои персональные данные']")))
-
         # Проверь, что текущий class div равен 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
         assert open_webdriver.find_element(By.XPATH, "//span[text()='Булки']/parent::div").get_attribute('class') == 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
+        close_webdriver
 
+    def test_constructor_sousi_transfer_ok(self, open_webdriver, close_webdriver):
+        #открываем главную страницу
+        open_webdriver.get(constant_data.BASE_URL)
+        # иммитируем переход на раздел Булки
+        open_webdriver.find_element(By.XPATH, "//span[text()='Соусы']").click()
+        # Проверь, что текущий class div равен 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
+        assert open_webdriver.find_element(By.XPATH, "//span[text()='Соусы']/parent::div").get_attribute('class') == 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
+        close_webdriver
+
+    def test_constructor_nachinki_transfer_ok(self, open_webdriver, close_webdriver):
+        #открываем главную страницу
+        open_webdriver.get(constant_data.BASE_URL)
+        # иммитируем переход на раздел Булки
+        open_webdriver.find_element(By.XPATH, "//span[text()='Начинки']").click()
+        # Проверь, что текущий class div равен 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
+        assert open_webdriver.find_element(By.XPATH, "//span[text()='Начинки']/parent::div").get_attribute('class') == 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
         close_webdriver
