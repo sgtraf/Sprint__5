d1fde59a37c7e2231aaaedd3e2438ebe27d44eeb
G Sh <conan@yandex.ru>
2025-03-25 20:11:52 +0300

    Исправил conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-
 import constant_data
 from locators import Locators
-
 
 @pytest.fixture
 #открывает вебдрайвер
@@ -25,15 +23,14 @@ def enter_to_site(open_webdriver):
     # открываем главную страницу
     open_webdriver.get(constant_data.BASE_URL)
     # нажимаем на кнопку Войти на главной странице
-    open_webdriver.find_element(By.CSS_SELECTOR, ".button_button__33qZ0").click()
+    open_webdriver.find_element(*Locators.BT_ENTER).click()
 
     # Добавь явное ожидание для загрузки страницы (проверяем появление надписи "Забыли пароль?")
     WebDriverWait(open_webdriver, 3).until(
-        expected_conditions.visibility_of_element_located((By.XPATH, "/html/body/div/div/main/div/div/p[2]")))
+        expected_conditions.visibility_of_element_located((By.XPATH, Locators.TEXT_FORGGOT_PASS_LP)))
     # Заполняем поля своим логином и паролем
-    open_webdriver.find_element(By.XPATH, "//div[label[contains(text(),'Email')]]//input").send_keys(
+    open_webdriver.find_element(*Locators.INPUT_EMAIL).send_keys(
         constant_data.LOGIN)
-    open_webdriver.find_element(By.XPATH, "//div[label[contains(text(),'Пароль')]]//input").send_keys(
+    open_webdriver.find_element(*Locators.INPUT_PASSW).send_keys(
         constant_data.PASSWORD)
     open_webdriver.find_element(*Locators.REG_ENTER).click()
-
